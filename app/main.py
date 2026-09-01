d5b47fc148a1b3f012474f03f41a65bc60926836
M. Sabtain Khan <msabtainkhan131@gmail.com>
2026-09-01 16:06:50 +0500

    Link deployed container in both backend and frontend

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://lumeluxe-chatbot-6fe84.containers.snapdeploy.app",
         # TODO Phase C: add your standalone demo's deployed URL once you have it
         # TODO Phase D: add "https://lumeluxe.pk" once client approves integration
     ],
